c30b02a3b03f1bd5d6a5393f96190c34a32316cc
Actuweary <85460865+Actuweary@users.noreply.github.com>
2022-02-16 15:22:17 -0500

    Add files via upload

---
+++ DSC530_M_ERSEVIM.py
@@ -0,0 +1,20 @@
+#DSC 530 Week 2
+#Bellevue University - FAll 2021
+#Michael Ersevim
+
+x = 23
+y = 52
+first = "This is the first sentence."
+second = "And this is the second sentence!"
+print('Hello World! I wonder why that is always the default coding text to start with?')
+print(x+y)
+print(x-y)
+print(x*y)
+print(x/y)
+print(first, second)
+listof4 = ["Cat", "Painting", 5, [1,2,3]]
+print(listof4)
+listof4.append("Blue")
+print(listof4)
+tpl = ('grams', -25, 43, 2000)
+print(tpl)
